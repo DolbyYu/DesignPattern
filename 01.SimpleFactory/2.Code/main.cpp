f6b83e9dba09f212cfb68c067816b2b72b1293cb
Dolby <yuaisheng@live.com>
2022-11-19 17:47:34 +0800

    add comments in main

--- 01.SimpleFactory/2.Code/main.cpp
+++ 01.SimpleFactory/2.Code/main.cpp
@@ -5,6 +5,9 @@
 int main()
 {
 	printf("简单工厂模式\n");
+
+	//! AbstractSportProduct 作为父类，派生多个子类。
+	//! Factory 用于根据输入参数，返回不同的子类实例
 	
 	//定义工厂类对象
 	std::shared_ptr<Factory> fac = std::make_shared<Factory>();
